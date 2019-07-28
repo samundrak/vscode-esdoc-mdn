4d71ee5e2a219858c2aef91dba47a0ab2289a037
rs-hub <rsaryev@icloud.com>
2019-07-28 09:47:58 +0300

    add switch language and add link or reduce doc

--- src/helper.js
+++ src/helper.js
@@ -1,7 +1,11 @@
+const vscode = require('vscode');
+
+//mdn object;
 module.exports = {
   getUrlFromToken(token, urlToDocs) {
     const [globalObject, method] = token.split('.');
-    const urlToGlobalObjectDoc = urlToDocs[(globalObject || '').toLowerCase()];
+    const lang = vscode.workspace.getConfiguration().get('esdoc.language');
+    const urlToGlobalObjectDoc = urlToDocs[(globalObject || '').toLowerCase()].replace('en-US', lang);
     if (!urlToGlobalObjectDoc) return;
     const url = method
       ? `${urlToGlobalObjectDoc}/${method}`
